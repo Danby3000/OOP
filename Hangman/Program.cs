ea7c126660eba5d25ef903086f7adcf3a348e1e4
psb18130 <50241330+Danby3000@users.noreply.github.com>
2023-06-16 13:24:00 +0100

    guessing and cli almost done, need to randomise words

---
+++ Hangman/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int guesses = 0;
+            WordCheck wc = new WordCheck();
+
+            Console.WriteLine("Welcome, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Well {0}, this is the hangmans office and you're a fixin for todays hangin'", name);
+            Console.WriteLine("Below I will display an empty word and you have to guess what it is letter by letter. You have 7 goes\n");
+
+            //generate word and placeholder
+
+            string word = "banana";
+            char[] placeholder = "------".ToCharArray();
+
+            
+
+            do
+            {
+                Console.WriteLine(new string(placeholder)+ "\n");
+                Console.WriteLine("What is your guess?");
+                String guessIn = Console.ReadLine();
+
+                while (String.IsNullOrWhiteSpace(guessIn))
+                {
+                    Console.WriteLine("Input is null or empty...");
+                    guessIn = Console.ReadLine();
+                }
+                while (guessIn.All(char.IsDigit))
+                {
+                    Console.WriteLine("That is a number...");
+                    guessIn = Console.ReadLine();
+                }
+                while (guessIn.Length > 1)
+                {
+                    Console.WriteLine("Only ONE letter please...");
+                    guessIn = Console.ReadLine();
+                }
+
+                char guess = (guessIn.ToLower()).ToCharArray()[0];
+
+                if (wc.InWord(word, guess))
+                {
+                    ArrayList arr = wc.CharPositions(word, guess);
+                    foreach(int i in arr)
+                    {
+                        placeholder[i] = guess;
+                    }
+                }
+
+
+
+
+
+            } while (guesses < 7);
+        }
+    }
+}
---
+++ Hangman/WordCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class WordCheck
+    {
+        public bool InWord(string word, char letter)
+        {
+            return word.Contains(letter);
+        }
+
+        public ArrayList CharPositions(string word, char letter) 
+        {
+            bool allFound = false;
+            ArrayList indexes = new ArrayList();
+
+            int firstDex = word.IndexOf(letter);
+            int lastDex = word.LastIndexOf(letter);
+
+            while (!allFound)
+            {
+                if (firstDex == lastDex)
+                {
+                    indexes.Add(firstDex);
+                    allFound = true;
+                } else
+                {
+                    indexes.Add(firstDex);
+                    int startpos = firstDex + 1;
+                    firstDex = word.IndexOf(letter, startpos);
+                }
+            }
+            return indexes;
+        }
+
+        public bool IsComplete(string word)
+        {
+            return (!word.Contains('_'));
+        }
+
+
+    }
+}
